798b191f7143c674eef1ebf3027c1301738913b0
supunyasantha <supunyasantha.official@gmail.com>
2025-06-14 13:44:53 +0530

    uncomment code

--- screens/Category.js
+++ screens/Category.js
@@ -1,27 +1,27 @@
-// import React from "react";
-// import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
 
-// export default function Category({ route }) {
-//   const { categoryName } = route.params;
+export default function Category({ route }) {
+  const { categoryName } = route.params;
 
-//   return (
-//     <View style={styles.container}>
-//       <Text style={styles.title}>{categoryName} Category</Text>
-//     </View>
-//   );
-// }
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>{categoryName} Category</Text>
+    </View>
+  );
+}
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#2E2E2E",
-//     padding: 20,
-//     justifyContent: "center",
-//     alignItems: "center",
-//   },
-//   title: {
-//     fontSize: 24,
-//     fontWeight: "bold",
-//     color: "#FFFFFF",
-//   },
-// });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#2E2E2E",
+    padding: 20,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#FFFFFF",
+  },
+});
